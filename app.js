70c6f4fcaac9ec5261d9beb4b9cae6289eb05d03
nicolas <nicodyj01@gmail.com>
2021-09-16 00:05:01 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+//motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+//direccion statica que inicia el servidor en la carpeta public
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+    res.render("index", {titulo: "pagina de inicio"})
+})
+
+app.get('/ayuda', (req, res) => {
+    res.render("ayuda", {titulo: "ayuda"})
+})
+
+app.listen(port, () => {
+    console.log('servidor a su servicio en el puerto', port)
+})
---
+++ public/codigo.js
@@ -0,0 +1,30 @@
+
+
+//var mensaje = `hola mundo`;
+//alert(mensaje);
+
+const ultimo = document.querySelector(".ultimo");
+
+console.log(ultimo);
+
+
+/*
+class Textos{
+    constructor(){}
+
+    static h1(texto){
+        let titulo = `<h1>${texto}</h1>`
+        console.log(titulo);
+    }
+    static h2(texto){
+        let titulo = `<h2>${texto}</h2>`
+        console.log(titulo);
+    }
+}
+
+let nombre = 'nicolas';
+let saludo = `hola ${nombre}, buenas noches`
+
+Textos.h2(saludo);
+*/
+
